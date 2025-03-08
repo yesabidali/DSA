a22c8dac9e68d8bb48cc33183bbf338e2ab9b8c9
Abid Ali <abid86227@gmail.com>
2025-03-08 22:56:46 +0530

    Array

---
+++ Stack.py
@@ -0,0 +1,9 @@
+stack = []
+stack.append("Bus")
+stack.append("Car")
+stack.append("Bike")
+print("Stack after inserting elements", stack)
+print ("After removing elements from Stack:", stack.pop())
+print ("After removing elements from Stack:", stack.pop())
+print ("After removing elements from Stack:", stack.pop())
+print("Stack after poping all elements:", stack)
